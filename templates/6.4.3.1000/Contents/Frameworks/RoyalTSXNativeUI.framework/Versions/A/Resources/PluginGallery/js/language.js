12d250f3c3550416f8db972257ccb5105e75b998
朴圣佑 <yes-men@qq.com>
2026-07-22 13:46:42 +0800

    chore: normalize 6.4.3 template whitespace

--- templates/6.4.3.1000/Contents/Frameworks/RoyalTSXNativeUI.framework/Versions/A/Resources/PluginGallery/js/language.js
+++ templates/6.4.3.1000/Contents/Frameworks/RoyalTSXNativeUI.framework/Versions/A/Resources/PluginGallery/js/language.js
@@ -1,7 +1,7 @@
 var language = {
 	currentLanguage: "en",
 	currentLanguageContent: null,
-	
+
 	init: function (initialLanguage) {
 		language.activeLanguage(initialLanguage);
 	},
@@ -20,7 +20,7 @@ var language = {
 
 		return "en";
 	},
-	
+
 	activeLanguage: function (newActiveLanguage) {
 		if (newActiveLanguage) {
 			try {
@@ -32,34 +32,34 @@ var language = {
 				language.currentLanguageContent = eval("language_file_" + language.currentLanguage);
 			}
 		}
-		
+
 		return language.currentLanguage;
 	},
-	
+
 	get: function (text) {
 		if (language.currentLanguageContent == null ||
 			!language.currentLanguageContent.hasOwnProperty(text)) {
 			return text;
 		}
-			
+
 		return language.currentLanguageContent[text];
 	},
-	
+
 	formatString: function (str) {
 		for (i = 1; i < arguments.length; i++) {
 			str = str.replace("{" + (i - 1) + "}", arguments[i]);
 		}
-		
+
 		return str;
 	},
-	
+
 	getFormat: function (text, args) {
 		text = language.get(text);
-		
+
 		try {
 			text = language.formatString(text, args);
 		} catch (ex) { }
-		
+
 		return text;
 	}
 };
